31477b5f3725e57241942b5b4cc37eeacee9f8ca
matheus <Matheusrdpa@hotmail.com>
2026-04-09 12:11:08 -0300

    Reverse linked list

---
+++ ReverseLinkedList/src/main/java/org/Main.java
@@ -0,0 +1,20 @@
+package org;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+
+        while(current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+}
